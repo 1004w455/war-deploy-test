fe266a7afcf814fe4c85062446be1f2c63ca1514
SangQ <2014ksg@adwitt.com>
2017-06-09 20:54:50 +0900

    feat: 테스트 코드 작성

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +17,21 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	private LocalDateTime startTime;
+
+	@PostConstruct
+	public void init() {
+		this.startTime = LocalDateTime.now();
+	}
+
 	@RestController
 	public class Controller {
 		@GetMapping("/")
 		public String home() {
-			return "hello world";
+			System.out.println("호출됨: " +
+					LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초")));
+			return String.format("war deploy success (서버시작시간: %s)"
+					, startTime.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초")));
 		}
 	}
 
